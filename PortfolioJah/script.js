b93d10d060f30bdc905ed569e46cb47aa9b16307
yahyobek <yahyobek.madrimov@gmail.com>
2026-05-18 12:09:45 +0300

    Js muuttujat valmis

---
+++ PortfolioJah/script.js
@@ -0,0 +1,17 @@
+let a = 3;
+let b = 5;
+let c = 7;
+let d = 15;
+
+function summa () {
+    alert(a+b);
+}
+function vahennys () {
+    alert(c-b);
+}
+function kerto () {
+    alert(a*b);
+}
+function jako () {
+    alert(d/b);
+}
