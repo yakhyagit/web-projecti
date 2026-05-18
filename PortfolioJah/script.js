a86630962a63d186e427aa79473ea9c3970cadd6
yahyobek <yahyobek.madrimov@gmail.com>
2026-05-18 12:30:29 +0300

    js numerot valmis

--- PortfolioJah/script.js
+++ PortfolioJah/script.js
@@ -3,15 +3,35 @@ let b = 5;
 let c = 7;
 let d = 15;
 
-function summa () {
-    alert(a+b);
+function summa (eka, toka) {
+    alert(eka + toka);
 }
-function vahennys () {
-    alert(c-b);
+function vahennys (eka, toka) {
+    alert(eka - toka);
 }
-function kerto () {
-    alert(a*b);
+function kerto (eka, toka) {
+    alert(eka * toka);
 }
-function jako () {
-    alert(d/b);
+function jako (eka, toka) {
+    alert(eka / toka);
+}
+function laskin_summa() { 
+    let eka = Number(document.getElementById("numero1").value);
+    let toka = Number(document.getElementById("numero2").value)
+    summa(eka, toka)
+}
+function laskin_vahennys() { 
+    let eka = Number(document.getElementById("numero1").value);
+    let toka = Number(document.getElementById("numero2").value);
+    vahennys(eka, toka)
+}
+function laskin_kerto() { 
+    let eka = Number(document.getElementById("numero1").value);
+    let toka = Number(document.getElementById("numero2").value);
+    kerto(eka, toka)
+}
+function laskin_jako() { 
+    let eka = Number(document.getElementById("numero1").value);
+    let toka = Number(document.getElementById("numero2").value);
+    jako(eka, toka)
 }
